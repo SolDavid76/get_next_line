6dce52a126cb3fee1e0c3d0c60d8676502506710
SolDavid76 <david.janusz75@gmail.com>
2022-12-19 21:17:13 +0100

    don't check this code

--- get_next_line.c
+++ get_next_line.c
@@ -15,27 +15,32 @@
 char	*get_next_line(int fd)
 {
 	static char	tmp[BUFFER_SIZE + 1];
-	char		*line;
+	char		*stash;
 	int			n;
 
 	n = 1;
-	line = NULL;
+	stash = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	if (ft_strchr(tmp, '\n'))
-		line = ft_endcpy(tmp);
-	while (!ft_strchr(line, '\n') && n != 0)
+		stash = ft_endcpy(tmp);
+	while (!ft_strchr(stash, '\n') && n != 0)
 	{
 		n = read(fd, tmp, BUFFER_SIZE);
 		if (n == -1)
-			return (free(line), NULL);
+			return (free(stash), NULL);
 		if (n == 0)
-			return (line);
+		{
+			if (ft_strlen(stash) == 0)
+				return (free(stash), NULL); //dumbest thing I'll never do
+			return (stash);
+		}
 		tmp[n] = '\0';
-		line = ft_strjoin(line, tmp);
-		if (!line)
+		stash = ft_strjoin(stash, tmp);
+		if (!stash)
 			return (NULL);
 	}
-	line = ft_get_line(line);
-	return (line);
+	return (ft_get_line(stash));
 }
 /*
 #include <fcntl.h>
@@ -46,7 +51,10 @@ int	ft_putstr(char *str)
 
 	i = 0;
 	if (!str)
+	{
+		write(1, "(null)\n", 7);
 		return (0);
+	}
 	while (str[i])
 		i+= write(1, str + i, 1);
 	return (i);
@@ -59,12 +67,13 @@ int	main(void)
 	char	*res;
 
 	fd = open("test.txt", O_RDONLY);
-	i = 1;
+	i = 2;
 	while (i)
 	{
 		res = get_next_line(fd);
-		i = ft_putstr(res);
+		ft_putstr(res);
 		free(res);
+		i--;
 	}
 }
 */
